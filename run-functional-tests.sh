185558ae612cc488be368e74a522718e5bdfc5b1
mmontpetit <micheal.montpetit@qlik.com>
2016-03-04 20:13:40 +0000

    test

--- run-functional-tests.sh
+++ run-functional-tests.sh
@@ -1,4 +1,13 @@
 echo "hello?"
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 
-
+ssh ubuntu@${DOCKER_HOST} \
+   docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+ssh ubuntu@${DOCKER_HOST} \
+   sudo mkdir -p ${SERVICE_DIR}
+ssh ubuntu@${DOCKER_HOST} \
+   sudo chown admin ${SERVICE_DIR}
+scp docker-compose.yml.deploy \
+    ubuntu@${DOCKER_HOST}:${SERVICE_DIR}/docker-compose.yml
+ssh ubuntu@${DOCKER_HOST} \
+    docker-compose -p g3cloudhub -f ${SERVICE_DIR}/docker-compose.yml up -d
